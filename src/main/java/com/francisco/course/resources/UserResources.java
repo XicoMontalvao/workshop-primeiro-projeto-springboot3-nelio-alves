5c00c9e47757ed82e15bf72c2db08337d1d5b451
Francisco Montalvao <f.montalvao@outlook.com>
2025-10-20 03:13:04 -0300

    User entity and resource

---
+++ src/main/java/com/francisco/course/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.francisco.course.resources;
+
+import com.francisco.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "maria", "maria@gmail.com", "9999999","123124");
+
+        return ResponseEntity.ok(u);
+    }
+}
