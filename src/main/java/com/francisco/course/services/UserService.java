3e805c8053acb3f2eefeb27dd0c7c30f933a9cda
Francisco Montalvao <f.montalvao@outlook.com>
2025-10-21 04:19:13 -0300

    User update

--- src/main/java/com/francisco/course/services/UserService.java
+++ src/main/java/com/francisco/course/services/UserService.java
@@ -29,4 +29,14 @@ public class UserService {
     public void delete(Long id){
         repositoy.deleteById(id);
     }
+    public User update(Long id, User user){
+        User entity = repositoy.getReferenceById(id);
+        updateData(entity, user);
+        return repositoy.save(entity);
+    }
+    private void updateData(User entity, User user){
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
 }
